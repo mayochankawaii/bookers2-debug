f15506a18c3e29044d52cb3c99cfe24c53c5179c
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-07-01 16:13:32 +0000

    bookers2 debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(current_user)
   end
-
+  
   def after_sign_out_path_for(resource)
     root_path
   end
