8de773589fb3155b7413201ea6fc114d28030c30
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-07-02 06:36:09 +0000

    bookers2 debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
   end
@@ -23,7 +24,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
@@ -34,5 +34,4 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
-end
 end
